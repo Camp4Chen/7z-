845fcea7b5b5be7b583b890b898afb212aeb9fbb
Camp4Chen <cly19940305@163.com>
2019-09-09 22:53:10 +0800

    2019年9月9日22:53:07

---
+++ ZipDemo/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace ZipDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnZip = new System.Windows.Forms.Button();
+            this.btn_Unzip = new System.Windows.Forms.Button();
+            this.btn_html = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnZip
+            // 
+            this.btnZip.Location = new System.Drawing.Point(12, 12);
+            this.btnZip.Name = "btnZip";
+            this.btnZip.Size = new System.Drawing.Size(75, 23);
+            this.btnZip.TabIndex = 0;
+            this.btnZip.Text = "压缩文件";
+            this.btnZip.UseVisualStyleBackColor = true;
+            this.btnZip.Click += new System.EventHandler(this.BtnZip_Click);
+            // 
+            // btn_Unzip
+            // 
+            this.btn_Unzip.Location = new System.Drawing.Point(93, 12);
+            this.btn_Unzip.Name = "btn_Unzip";
+            this.btn_Unzip.Size = new System.Drawing.Size(75, 23);
+            this.btn_Unzip.TabIndex = 1;
+            this.btn_Unzip.Text = "解压";
+            this.btn_Unzip.UseVisualStyleBackColor = true;
+            this.btn_Unzip.Click += new System.EventHandler(this.Btn_Unzip_Click);
+            // 
+            // btn_html
+            // 
+            this.btn_html.Location = new System.Drawing.Point(175, 11);
+            this.btn_html.Name = "btn_html";
+            this.btn_html.Size = new System.Drawing.Size(86, 23);
+            this.btn_html.TabIndex = 2;
+            this.btn_html.Text = "压缩html文件";
+            this.btn_html.UseVisualStyleBackColor = true;
+            this.btn_html.Click += new System.EventHandler(this.Btn_html_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_html);
+            this.Controls.Add(this.btn_Unzip);
+            this.Controls.Add(this.btnZip);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnZip;
+        private System.Windows.Forms.Button btn_Unzip;
+        private System.Windows.Forms.Button btn_html;
+    }
+}
+
---
+++ ZipDemo/Form1.cs
@@ -0,0 +1,116 @@
+﻿using SevenZip;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZipDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnZip_Click(object sender, EventArgs e)
+        {
+            string path = @"20170605-H3C S12500X-AF系列交换机 软件升级指南-6W101";
+            var rootPath = @"D:\work_file\demo_file";
+            var sourceDir = Path.Combine(rootPath, path);
+            if (Environment.Is64BitProcess)
+            {
+                SevenZipBase.SetLibraryPath(string.Format(@"{0}\x64\7z.dll", AppDomain.CurrentDomain.BaseDirectory));
+            }
+            else
+            {
+                SevenZipBase.SetLibraryPath(string.Format(@"{0}\x86\7z.dll", AppDomain.CurrentDomain.BaseDirectory));
+            }
+            var compressor = new SevenZipCompressor();
+            compressor.ArchiveFormat = OutArchiveFormat.SevenZip;
+            compressor.CompressionMode = CompressionMode.Create;
+            compressor.CompressDirectory(sourceDir, sourceDir + ".7z");
+        }
+
+        private void Btn_Unzip_Click(object sender, EventArgs e)
+        {
+            string zipPath = @"D:\work_file\demo_file\20170605-H3C S12500X-AF系列交换机 软件升级指南-6W101.7z";
+            DirectoryInfo directoryInfo = new DirectoryInfo(zipPath);
+            FileInfo file = new FileInfo(zipPath);
+            string destPath = @"D:\work_file\demo_file\Temp";
+            if (Environment.Is64BitProcess)
+            {
+                SevenZipBase.SetLibraryPath(string.Format(@"{0}\x64\7z.dll", AppDomain.CurrentDomain.BaseDirectory));
+            }
+            else
+            {
+                SevenZipBase.SetLibraryPath(string.Format(@"{0}\x86\7z.dll", AppDomain.CurrentDomain.BaseDirectory));
+            }
+            if (file.Exists)
+            {
+                using (SevenZipExtractor tmp = new SevenZipExtractor(zipPath))
+                {
+                    tmp.ExtractArchive(Path.Combine(destPath, file.Name.Replace(directoryInfo.Extension, "")));
+                }
+            }
+        }
+
+        private void Btn_html_Click(object sender, EventArgs e)
+        {
+            string path = @"D:\work_file\demo_file\Temp\20170605-H3C S12500X-AF系列交换机 软件升级指南-6W101";
+            string destPath = @"D:\work_file\demo_file\Document";
+            DirectoryInfo directoryInfo = new DirectoryInfo(path);
+            var htmls = directoryInfo.GetFiles("html.*");
+            if (htmls.Length == 1)
+            {
+                UnZipHtml(htmls[0].FullName, directoryInfo.Name, destPath);
+                ZipHtml(destPath, directoryInfo.Name);
+            }
+        }
+
+        private void ZipHtml(string destPath, string name)
+        {
+            var sourceDir = Path.Combine(destPath, name,"html");
+            var fileName = Path.Combine(destPath,name);
+            if (Environment.Is64BitProcess)
+            {
+                SevenZipBase.SetLibraryPath(string.Format(@"{0}\x64\7z.dll", AppDomain.CurrentDomain.BaseDirectory));
+            }
+            else
+            {
+                SevenZipBase.SetLibraryPath(string.Format(@"{0}\x86\7z.dll", AppDomain.CurrentDomain.BaseDirectory));
+            }
+            var compressor = new SevenZipCompressor();
+            compressor.ArchiveFormat = OutArchiveFormat.SevenZip;
+            compressor.CompressionMode = CompressionMode.Create;
+            compressor.CompressDirectory(sourceDir, fileName + ".7z");
+        }
+
+        private void UnZipHtml(string fullPath, string name, string destPath)
+        {
+            DirectoryInfo directory = new DirectoryInfo(fullPath);
+            FileInfo file = new FileInfo(fullPath);
+            if (Environment.Is64BitProcess)
+            {
+                SevenZipBase.SetLibraryPath(string.Format(@"{0}\x64\7z.dll", AppDomain.CurrentDomain.BaseDirectory));
+            }
+            else
+            {
+                SevenZipBase.SetLibraryPath(string.Format(@"{0}\x86\7z.dll", AppDomain.CurrentDomain.BaseDirectory));
+            }
+            if (file.Exists)
+            {
+                using (SevenZipExtractor tmp = new SevenZipExtractor(fullPath))
+                {
+                    tmp.ExtractArchive(Path.Combine(destPath, name));
+                }
+            }
+        }
+    }
+}
